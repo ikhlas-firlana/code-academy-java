8a630c30bfbab23fc33dd0d857153f65c9fa5e16
ifirlana <ifirlana@gmail.com>
2025-05-25 20:11:16 +0700

    learn

---
+++ projects/planting-a-tree/Tree.java
@@ -0,0 +1,13 @@
+// Define your tree class in this file!
+public class Tree {
+    // introduce tree
+    public static void main(String[] args) {
+        System.out.println("Hey there, I’m Ariel and I’m learning to code in Java!");
+        System.out.println("I’m going to plant a tree today!” or “Ready to get my hands dirty!");
+        System.out.println("  *  ");
+        System.out.println(" *** ");
+        System.out.println(" *** ");
+        System.out.println("  *  ");
+        System.out.println("  *  ");
+    }
+}
